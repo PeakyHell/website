00dc46c6100435e258f16d4492b7995ce35e581c
PeakyHell <contact@peakyhell.com>
2025-09-07 18:00:34 +0200

    Add error handling for YouTube downloader

--- routes/youtube_dl.py
+++ routes/youtube_dl.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, send_file
+from flask import Blueprint, render_template, request, send_file, redirect, url_for, make_response
 from yt_dlp import YoutubeDL
 from tempfile import TemporaryDirectory
 import os
@@ -10,7 +10,8 @@ youtube_downloader = Blueprint("Youtube Downloader", __name__)
 # Routes
 @youtube_downloader.get("/")
 def get_yt_dl():
-    return render_template("yt-dl.html")
+    error = request.cookies.get("yt-dl-error")
+    return render_template("yt-dl.html", error=error)
 
 
 @youtube_downloader.post("/")
@@ -20,12 +21,20 @@ def post_yt_dl():
         output_file = os.path.join(tmp_dir, "download.mp4")
 
         yt_options = {
+            # TODO : Implement cookies for authentication
             "format": "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b", # Download the best mp4 video available, or the best video if no mp4 available
             "merge_output_format": "mp4",
             "outtmpl": output_file
         }
 
         with YoutubeDL(yt_options) as yt_dl:
-            yt_dl.download(request.form["link"])
+            try:
+                yt_dl.download(request.form["link"])
+            except:
+                res = make_response(redirect(url_for("Youtube Downloader.get_yt_dl")))
+                res.set_cookie("yt-dl-error", "Could not download this video. Make sure the link is correct or retry later.")
+                return res
 
-        return send_file(output_file, as_attachment=True)
+        res = make_response(send_file(output_file, as_attachment=True))
+        res.set_cookie("yt-dl-error", None)
+        return res
