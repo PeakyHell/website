f17cc48051a38b0f239ff941e5048f8393a7fcd8
PeakyHell <contact@peakyhell.com>
2025-09-10 15:54:15 +0200

    Import docker secrets for Bitwarden Secrets

--- modules/secrets.py
+++ modules/secrets.py
@@ -1,12 +1,15 @@
+import os
 from bitwarden_sdk import BitwardenClient, DeviceType, client_settings_from_dict
 
 client = BitwardenClient(
     client_settings_from_dict(
         {
-            "apiUrl": "",
+            "apiUrl": os.getenv("BW_API_URL"),
             "deviceType": DeviceType.SDK,
-            "identityUrl": "",
+            "identityUrl": os.getenv("BW_IDENTITY_URL"),
             "userAgent": "Python"
         }
     )
 )
+
+client.auth().login_access_token(os.getenv("BW_ACCESS_TOKEN"))
