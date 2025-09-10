08f03a546a11bea0251fb761c898d8e4df75ef5c
PeakyHell <contact@peakyhell.com>
2025-09-10 16:18:43 +0200

    Fix usage of environment variables

--- modules/secrets.py
+++ modules/secrets.py
@@ -1,15 +1,19 @@
 import os
 from bitwarden_sdk import BitwardenClient, DeviceType, client_settings_from_dict
 
+with open(os.getenv("BW_API_URL")) as f: BW_API_URL=f.read().strip()
+with open(os.getenv("BW_IDENTITY_URL")) as f: BW_IDENTITY_URL=f.read().strip()
+with open(os.getenv("BW_ACCESS_TOKEN")) as f: BW_ACCESS_TOKEN=f.read().strip()
+
 client = BitwardenClient(
     client_settings_from_dict(
         {
-            "apiUrl": os.getenv("BW_API_URL"),
+            "apiUrl": BW_API_URL,
             "deviceType": DeviceType.SDK,
-            "identityUrl": os.getenv("BW_IDENTITY_URL"),
+            "identityUrl": BW_IDENTITY_URL,
             "userAgent": "Python"
         }
     )
 )
 
-client.auth().login_access_token(os.getenv("BW_ACCESS_TOKEN"))
+client.auth().login_access_token(BW_ACCESS_TOKEN)
